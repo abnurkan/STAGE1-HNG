4fb6b84f8c75800ac95c27bac67a1ffc6b73f2ca
abnurkan <59101298+abnurkan@users.noreply.github.com>
2024-07-02 19:01:28 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -4,10 +4,6 @@ from django.conf import settings
 import requests
 import json
 
-
-
-
-
 def get_weather(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={settings.OPENWEATHERMAP_API_KEY}'
     response = requests.get(url)
@@ -16,18 +12,18 @@ def get_weather(city):
 
 def help(request):
     visitor_name = request.GET.get('visitor_name', 'Guest')
-    client_ip = request.META.get('REMOTE_ADDR')
-
     ip=requests.get('https://api.ipify.org?format=json')
     ip_data = json.loads(ip.text)
     res =requests.get('http://ip-api.com/json/'+ip_data["ip"])
+
+
+    c= ip_data["ip"]
 
     location_data_one=res.text
     location_data = json.loads(location_data_one)
 
 
     # Get geolocation data
-    
     city = location_data['city']
 
     # Get weather data
@@ -37,7 +33,7 @@ def help(request):
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
 
     response = {
-        "client_ip": client_ip,
+        "client_ip": c,
         "location": city,
         "greeting": greeting
     }
