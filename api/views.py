564e28b34218801a1bb1c8539f09240106982c70
abnurkan <59101298+abnurkan@users.noreply.github.com>
2024-07-04 02:41:31 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.conf import settings
-import requests
 import json
-import geocoder
-from ipware import get_client_ip
-
+import requests
+from django.http import JsonResponse
 
 
 def get_weather(city):
@@ -13,30 +11,33 @@ def get_weather(city):
     response = requests.get(url)
     return response.json()
 
-
 def help(request):
-    visitor_name = request.GET.get('visitor_name', 'Guest')
+    visitor_name = request.GET.get('visitor_name')
+    # Get user IP address
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
 
-    client_ip, is_routable = get_client_ip(request)
+    try:
+        response = requests.get(f'http://ipinfo.io/{ip}?token=84476190df868e')
+        location_data = response.json()
+        city = location_data.get('city', 'Unknown')
+    except Exception as e:
+        city = "Unknown"
+        country = "Unknown"
 
-    ip_address = client_ip
-
-    url = f"http://ipinfo.io/{ip_address}?token=84476190df868e"
-    response = requests.get(url)
-    res = response.json()
-
-    city = res['city']
-
+    
     # Get weather data
     weather_data = get_weather(city)
     temperature = weather_data['main'].get('temp', 'N/A')
-
+    
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
-
+   
     response = {
-        "client_ip": ip_address,
-        "location": city,
-        "greeting": greeting
+        'client_ip': ip,
+        'city': city,
+        'greeting': greeting
     }
-
     return JsonResponse(response)
